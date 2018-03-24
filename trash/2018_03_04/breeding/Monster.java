1131539d5045323a728d03f0503ecf25d34bf1ac
Saki Shoji <33520022+sakishoji@users.noreply.github.com>
2018-03-24 20:05:27 +0900

    Add files via upload

--- trash/2018_03_04/breeding/Monster.java
+++ trash/2018_03_04/breeding/Monster.java
@@ -5,103 +5,107 @@ package breeding;
  */
 
 public class Monster {
-	Monster(){
-	}
+
+    /** 名前 */
+    private String name;
+    /** 体力 */
+    private int hp;
+    /** 体力の最大値 */
+    private final int MAX_HP = 100;
+
+    /**
+     * 名前を取得
+     * @return name
+     */
+     public String getName() {
+        return name;
+    }
+
+    /**
+     * 名前をセット
+     * @param name
+     */
+     public void setName(String name) {
+        this.name = name;
+    }
+    
+    /**
+     * 体力を取得
+     * @return hp
+     */
+     public int getHp() {
+        return hp;
+    }
 	
-	Monster(String name, int hp) {
-		this.name = name;
-		this.hp = hp;
-	}
+    /**
+    * 体力をセット
+    * @param hp
+    */
+     public void setHp(int hp) {
+        this.hp = hp;
+     }
+     /**
+      * コンストラクタ
+      */
+     Monster(){
+     }
 
-	/** 名前 */
-	private String name;
-	/** 体力 */
-	private int hp;
-	/** 体力の最大値 */
-	private final int MAX_HP = 100;
+     Monster(String name, int hp) {
+         this.name = name;
+         this.hp = hp;
+     }
+     
+     /**
+       * 名前を出力
+       * @param name 私の名前
+       */
+     public void talk() {
+        System.out.println("こんちは！私の名前は、" + this.name + "だよ！");
+    }
 
-	/**
-	 * 名前を取得
-	 * @return name
-	 */
-	 public String getName() {
-		return name;
-	}
-	
-	/**
-	 * 名前をセット
-	 * @param name
-	 */
-	 public void setName(String name) {
-		this.name = name;
-	}
-	
-	/**
-	 * 体力を取得
-	 * @return hp
-	 */
-	 public int getHp() {
-		return hp;
-	}
-	
-	/**
-	 * 体力をセット
-	 * @param hp
-	 */
-	 public void setHp(int hp) {
-		this.hp = hp;
-	}
+     /**
+      * 歩いた分体力を消費
+      * @param minute 歩いた分数
+      */
+     public void walk(int minute) {
+        System.out.println(minute + "分散歩したよ！");
+        this.hp -= minute * 2;
+        if (this.hp <= 50) {
+            System.out.println("お腹すいたなぁ、、");
+        }
+     }
 
-	/**
-	 * 名前を出力
-	 * @param name 私の名前
-	 */
-	 public void talk() {
-		System.out.println("こんちは！私の名前は、" + this.name + "だよ！");
-	}
+     /**
+      * 食べ物を食べて回復
+      * @param food 食べ物
+      * @return 感謝の言葉
+      */
+     public String eat(String food) {
+        if ("やきにく".equals(food)) {
+            String a = "おおおぉぉ!!!!!" + food + "!!!!! ありがとう!!!!!";
+            this.hp += 100;
+            return a;
+        } else {
+            String b = "おぉ," + food + "をくれるの？ ありがとう!";
+            this.hp += 10;
+            return b;
+        }
+    }
 
-	/**
-	 * 歩いた分体力を消費
-	 * @param minute 歩いた分数
-	 */
-	 public void walk(int minute) {
-		System.out.println(minute + "分散歩したよ！");
-		this.hp -= minute * 2;
-		if (this.hp <= 50) {
-			System.out.println("お腹すいたなぁ、、");
-		}
-	}
-
-	/**
-	 * 食べ物を食べて回復
-	 * @param food 食べ物
-	 * @return 感謝の言葉
-	 */
-	public String eat(String food) {
-		if ("やきにく".equals(food)) {
-			System.out.println("おおおぉぉ!!!!!" + food + "!!!!! ありがとう!!!!!");
-			this.hp += 100;
-		} else {
-			System.out.println("おぉ," + food + "をくれるの？ ありがとう!");
-			this.hp += 10;
-		}
-		return food;
-	}
-
-	/**
-	 * やくそうで回復
-	 * @param plant やくそう
-	 */
-	 public void cure(String plant) {
-		if (("やくそう".equals(plant) && 70 <= this.hp) || ("特やくそう".equals(plant))) {
-			this.hp = MAX_HP;
-			System.out.println("わーい！全回復！");
-		} else if ("やくそう".equals(plant) && 20 <= this.hp && this.hp < 70) {
-			this.hp += 30;
-			System.out.println("全回復ではない！");
-		} else if ("やくそう".equals(plant)) {
-			this.hp += 30;
-			System.out.println("どうも。");
-		}
-	}
+     /**
+      * やくそうで回復
+      * @param plant やくそう
+      */
+     public void cure(String plant) {
+        if (("やくそう".equals(plant) && 70 <= this.hp) || ("特やくそう".equals(plant))) {
+            this.hp = MAX_HP;
+            System.out.println("わーい！全回復！");
+        } else if ("やくそう".equals(plant) && 20 <= this.hp && this.hp < 70) {
+            this.hp += 30;
+            System.out.println("全回復ではない！");
+        } else if ("やくそう".equals(plant)) {
+            this.hp += 30;
+            System.out.println("どうも。");
+        }
+    }
 }
