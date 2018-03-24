d370fb00957950e017ab3a8b14e79b9340bfaff6
Saki Shoji <33520022+sakishoji@users.noreply.github.com>
2018-03-24 19:59:19 +0900

    Add files via upload

--- trash/2018_03_04/breeding/Main.java
+++ trash/2018_03_04/breeding/Main.java
@@ -1,35 +1,35 @@
 package breeding;
 
 public class Main {
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		Monster mon = new Monster();
-		mon.setName("なほみん");
-		mon.setHp(100);
-		System.out.println("名前：" + mon.getName() + " " + "HP：" + mon.getHp());
+        Monster mon = new Monster();
+        mon.setName("なほみん");
+        mon.setHp(100);
+        System.out.println("名前：" + mon.getName() + " " + "HP：" + mon.getHp());
 
-		mon.talk();
-		mon.walk(2);
-		System.out.println("名前：" + mon.getName() + " " + "HP：" + mon.getHp());
+        mon.talk();
+        mon.walk(2);
+        System.out.println("名前：" + mon.getName() + " " + "HP：" + mon.getHp());
 
-		mon.walk(10);
-		System.out.println("名前：" + mon.getName() + " " + "HP：" + mon.getHp());
+        mon.walk(10);
+        System.out.println("名前：" + mon.getName() + " " + "HP：" + mon.getHp());
 
-		mon.eat("サラダ");
-		System.out.println("名前：" + mon.getName() + " " + "HP：" + mon.getHp());
+        System.out.println(mon.eat("サラダ"));
+        System.out.println("名前：" + mon.getName() + " " + "HP：" + mon.getHp());
 
-		mon.eat("やきにく");
-		System.out.println("名前：" + mon.getName() + " " + "HP：" + mon.getHp());
+        System.out.println(mon.eat("やきにく"));
+        System.out.println("名前：" + mon.getName() + " " + "HP：" + mon.getHp());
 
-		mon.cure("やくそう");
-		System.out.println("名前：" + mon.getName() + " " + "HP：" + mon.getHp());
+        mon.cure("やくそう");
+        System.out.println("名前：" + mon.getName() + " " + "HP：" + mon.getHp());
 
-		mon.cure("特やくそう");
-		System.out.println("名前：" + mon.getName() + " " + "HP：" + mon.getHp());
+        mon.cure("特やくそう");
+        System.out.println("名前：" + mon.getName() + " " + "HP：" + mon.getHp());
 
-		mon.cure("くさ");
-		System.out.println("名前：" + mon.getName() + " " + "HP：" + mon.getHp());
-	}
+        mon.cure("くさ");
+        System.out.println("名前：" + mon.getName() + " " + "HP：" + mon.getHp());
+    }
 }
 //リファクタリング→低数値に切り出す。
 //　　　　　　　　→名前、HPの出力処理メソッド化。
